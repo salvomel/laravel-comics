b04274255a1d138776a371de33f79b74822e8e99
salvomel <salv.mel@gmail.com>
2022-02-09 16:46:04 +0100

    main/comics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,3 +3,9 @@
 @section('page_title')
 Comics
 @endsection
+
+@section('main_content')
+    
+    @include('partials.main')
+
+@endsection
---
+++ resources/views/partials/main.blade.php
@@ -0,0 +1,17 @@
+<main>
+    <section>
+        <div class="container">
+            <div class="comics">
+                @foreach($comics as $comic)
+                    <div class="comic">
+                        <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}">
+                        <h3>{{$comic['title']}}</h3>
+                    </div>
+                @endforeach
+            </div>
+            <div class="main-button">
+                <a href="#">LOAD MORE</a>
+            </div>
+        </div>
+    </section>
+</main>
