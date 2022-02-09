a980763b09411d75375fc7d8c0efa69855e0d968
salvomel <salv.mel@gmail.com>
2022-02-09 17:42:59 +0100

    finish

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -1,17 +1,15 @@
 <main>
-    <section>
-        <div class="container">
-            <div class="comics">
-                @foreach($comics as $comic)
-                    <div class="comic">
-                        <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}">
-                        <h3>{{$comic['title']}}</h3>
-                    </div>
-                @endforeach
-            </div>
-            <div class="main-button">
-                <a href="#">LOAD MORE</a>
-            </div>
+    <div class="container">
+        <div class="comics">
+            @foreach($comics as $comic)
+                <div class="comic">
+                    <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}">
+                    <h3>{{$comic['title']}}</h3>
+                </div>
+            @endforeach
         </div>
-    </section>
+        <div class="main-button">
+            <a href="#">LOAD MORE</a>
+        </div>
+    </div>
 </main>
