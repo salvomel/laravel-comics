a451ae8fa07bd8a4fac1ab06879be87e7ed7a348
salvomel <salv.mel@gmail.com>
2022-02-09 15:44:08 +0100

    header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,52 @@
+<header>
+
+    <div class="top-header">
+        <div class="container">
+            <span>DC POWER VISA</span>
+            <span>ADDITIONAL DC SITES</span>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="header-wrapper">
+            <div class="logo">
+                <img src="{{ asset('images/dc-logo.png') }}" alt="Logo DC">
+            </div>
+    
+            <nav>
+                <ul class="menu">
+                   <li>
+                        <a href="#">Characters</a>
+                    </li>
+                   <li class="current">
+                        <a href="#">Comics</a>
+                    </li>
+                   <li>
+                        <a href="#">Movies</a>
+                    </li>
+                   <li>
+                        <a href="#">Tv</a>
+                    </li>
+                   <li>
+                        <a href="#">Games</a>
+                    </li>
+                   <li>
+                        <a href="#">Collectibles</a>
+                    </li>
+                   <li>
+                        <a href="#">Videos</a>
+                    </li>
+                   <li>
+                        <a href="#">Fans</a>
+                    </li>
+                   <li>
+                        <a href="#">News</a>
+                    </li>
+                   <li>
+                        <a href="#">Shop</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
